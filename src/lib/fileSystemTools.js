fa572dc0d3a9b70b77cebfe27e588c2518cf8a06
Nando-C <73954373+Nando-C@users.noreply.github.com>
2021-06-24 18:29:24 +0100

    refactored Authors Router

--- src/lib/fileSystemTools.js
+++ src/lib/fileSystemTools.js
@@ -7,6 +7,7 @@ const { readJSON, writeJSON, writeFile } = fs
 const authorsJSONPath = join(dirname(fileURLToPath(import.meta.url)), '../data/authors.json')
 const postsJSONPath = join(dirname(fileURLToPath(import.meta.url)), '../data/posts.json')
 const blogPostsPublicFolderPath = join(dirname(fileURLToPath(import.meta.url)), '../../public/img/blogPosts')
+const authorsPublicFolderPath = join(dirname(fileURLToPath(import.meta.url)), '../../public/img/authors')
 
 export const getAuthorsArray = () => readJSON(authorsJSONPath)
 export const getPostsArray = () => readJSON(postsJSONPath)
@@ -17,3 +18,4 @@ export const writePosts = (content) => writeJSON(postsJSONPath, content)
 export const getCurrentFolderPath = (currentFile) => dirname(fileURLToPath(currentFile))
 
 export const writePostsImage = (fileName, content) => writeFile(join(blogPostsPublicFolderPath, fileName), content)
+export const writeAuthorsImage = (fileName, content) => writeFile(join(authorsPublicFolderPath, fileName), content)
--- src/services/authors/index.js
+++ src/services/authors/index.js
@@ -1,90 +1,115 @@
 import express from "express"
-import fs from "fs"
-import { fileURLToPath } from "url"
-import { dirname, join } from "path"
 import uniqid from "uniqid"
+import createError from 'http-errors'
 
+import { getAuthorsArray, writeAuthors, writeAuthorsImage } from '../../lib/fileSystemTools.js'
+import multer from 'multer'
 
 const authorsRouter = express.Router()
 
-const currentFilePath = fileURLToPath(import.meta.url)
-const currentFolderPath = dirname(currentFilePath)
-const authorsJSONPath = join(currentFolderPath, "authors.json")
-
 // GET /authors => returns the list of authors ============================
-authorsRouter.get("/", (req, res) => {
-
-    const authorsJSONContent = fs.readFileSync(authorsJSONPath)
-
-    res.send(JSON.parse(authorsJSONContent))
+authorsRouter.get("/", async (req, res, next) => {
+    try {
+        const authors = await getAuthorsArray()
+        res.send(authors)
+        
+    } catch (error) {
+        next(error)
+    }
 })
 
 // GET /authors/123 => returns a single author ============================
-authorsRouter.get("/:id", (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSONPath))
-    const author = authors.find(auth => auth._id === req.params.id)
-
-    res.send(author)
+authorsRouter.get("/:id", async (req, res, next) => {
+    
+    try {
+        const authors = await getAuthorsArray()
+        const author = authors.find(auth => auth._id === req.params.id)
+       
+        if(author) {
+            res.send(author)
+        } else {
+            next(createError(404, `Author with id ${req.params.id} not found!`))
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
 // POST /authors => creates a new author ===================================
-authorsRouter.post("/", (req, res) => {
+authorsRouter.post("/", async (req, res, next) => {
     const objModel = {
         name: "",
         surname: "",
         email: "",
         dateOfBirth: "",
     }
-    const newAvatar = `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`
-    const newAuthor = {...objModel, ...req.body, _id: uniqid(), avatar:newAvatar, createdAt: new Date()}
+    try {
+        const newAvatar = `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}`
+        const newAuthor = {_id: uniqid(), ...objModel, ...req.body, avatar:newAvatar, createdAt: new Date()}
+    
+        const authors = await getAuthorsArray()
+    
+        authors.push(newAuthor)
+    
+        await writeAuthors(authors)
+    
+        res.status(201).send({_id: newAuthor._id})
 
-    const authors = JSON.parse(fs.readFileSync(authorsJSONPath))
-
-    authors.push(newAuthor)
-
-    fs.writeFileSync(authorsJSONPath,JSON.stringify(authors))
-
-    res.status(201).send({_id: newAuthor._id})
-
+    } catch (error) {
+        next(error)
+    }
 })
 
 // PUT /authors/123 => edits the author with the given id ==================
-authorsRouter.put("/:id", (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSONPath))
-
-    const author = authors.find(auth => auth._id === req.params.id)
-
-    const remainingAuthors = authors.filter(auth => auth._id !== req.params.id)
-
-    const modifiedAuthor = {
-        ...author,
-        ...req.body,
-        _id: req.params.id, 
-        avatar: (req.body.name && req.body.surname) 
-        ? `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}` 
-        :  req.body.name 
-            ? `https://ui-avatars.com/api/?name=${req.body.name}+${author.surname}`
-            : req.body.surname
-                ? `https://ui-avatars.com/api/?name=${author.name}+${req.body.surname}`
-                : `https://ui-avatars.com/api/?name=${author.name}+${author.surname}`
+authorsRouter.put("/:id", async (req, res, next) => {
+    try {
+        const authors = await getAuthorsArray()
+        const author = authors.find(auth => auth._id === req.params.id)
+    
+        const remainingAuthors = authors.filter(auth => auth._id !== req.params.id)
+    
+        const modifiedAuthor = {
+            _id: req.params.id, 
+            ...author,
+            ...req.body,
+            avatar: (req.body.name && req.body.surname) 
+            ? `https://ui-avatars.com/api/?name=${req.body.name}+${req.body.surname}` 
+            :  req.body.name 
+                ? `https://ui-avatars.com/api/?name=${req.body.name}+${author.surname}`
+                : req.body.surname
+                    ? `https://ui-avatars.com/api/?name=${author.name}+${req.body.surname}`
+                    : `https://ui-avatars.com/api/?name=${author.name}+${author.surname}`
+        }
+    
+        remainingAuthors.push(modifiedAuthor)
+    
+        await writeAuthors(remainingAuthors)
+    
+        res.send(modifiedAuthor)
+        
+    } catch (error) {
+        next(error)
     }
-
-    remainingAuthors.push(modifiedAuthor)
-
-    fs.writeFileSync(authorsJSONPath, JSON.stringify(remainingAuthors))
-
-    res.send(modifiedAuthor)
 })
 
 // DELETE /authors/123 => deletes the author with the given id =============
-authorsRouter.delete("/:id", (req, res) => {
-    const authors = JSON.parse(fs.readFileSync(authorsJSONPath))
-    const remainingAuthors = authors.filter(auth => auth._id !== req.params.id)
+authorsRouter.delete("/:id", async (req, res, next) => {
+    try {
+        const authors = await getAuthorsArray()
+        const author = authors.find(auth => auth._id === req.params.id)
 
-    fs.writeFileSync(authorsJSONPath, JSON.stringify(remainingAuthors))
-
-    res.status(204).send()
-   
+        if(author) {
+            const remainingAuthors = authors.filter(auth => auth._id !== req.params.id)
+        
+            await writeAuthors(remainingAuthors)
+        
+            res.status(204).send()
+        } else {
+            next(createError(404, `Author with id ${req.params.id} not found!`))
+        }
+    } catch (error) {
+        next(error)
+    }
 })
 
 export default authorsRouter
